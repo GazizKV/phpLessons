3fcd2081e5e2cb139a6f7620858cd35b69cc5820
gazizkv <valitovgaziz@gmail.com>
2020-12-27 15:13:57 +0500

    1-3 lenssons

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+
+
+$name = "Andrei";
+$number = 3.14;
+
+$some = $second = $num_one = 1;
+
+echo $name;
+echo "<br>";
+$name = 33424;
+echo $name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+echo "<br>";
+include 'second.php';
+echo "<br>";
+echo "Some text\n";
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+// you can use same of the comand exp include or require
+// with require if the usees file not exit then that programm stack and
+// not execute
+// in use case with include program can execute without included file.*/
+?>
---
+++ second.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Запасной файл\n";
+echo "<br>";
+echo "Second file\n";
+
+?>
