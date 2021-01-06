a1ad17915858d8d47ef45352f7c20205c4739ed4
gazizkv <valitovgaziz@gmail.com>
2021-01-06 21:37:02 +0500

    ninth lesson required file and const in class defined by ::

--- index.php
+++ index.php
@@ -1,46 +1,24 @@
 <?php
-// define()
 
-define('PI', 3.14);
-define('number', 11);
 
-echo PI;
-echo "<br>";
-echo number;
-echo "<br>";
 
-if (defined('PI')) {
-	echo "const";
-} else {
-	echo "const not";
+echo "this index.php" . "<br>";
+
+// const of class
+class ConstantClass{
+	const NAME="const";
 }
-echo "<br>";
-if (defined('number')) {
-	echo "const";
-} else {
-	echo "const not";
-}
-echo "<br>";
-echo defined('PI');
-echo "<br>";
-/*
-__LINE__ current line 
-__FILE__ path current file name and full path
-__FUNCTION__ name of function
-__METHOD__ name method
-__CLASS__ name class
-__DIR__ current directory
-__PHP__VERSION version of PHP
-OS__VERSION os vers
-PHP__EOL  \n  \r\n
- */
+//echo ConstantClass::NAME;
+echo "<hr>";
 
-echo "files name " . __FILE__ . "\n";
-echo "<br>";
-echo __DIR__;
-echo "<br>";
-echo PHP_VERSION;
-echo "<br>";
-echo PHP_WINDOWS_EVENT_CTRL_C;
+if(defined('ConstantClass::NAME')){
+	echo 'const if define <br>';
+}
+else
+{
+	echo ' const not define';
+}
+
+
 
 ?>
