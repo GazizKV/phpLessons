e89aead5d4e73cc6fda29d5745b433e24633561d
gazizkv <valitovgaziz@gmail.com>
2021-01-25 21:00:07 +0500

    11 th lesson 	modified:   index.php

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
 
-$str1 = "It is good to learn ";
-$str2 = "PHP";
-$num = 123;
 
-$str = $str1 . $str2;
-
-echo $str;
 echo "<hr>";
-echo 5/3;
-echo "<br>";
-echo (int)(5/3);
-echo "<br>";
-echo 5%3;
 
+$number = 111;
+$var = 2;
+
+
+echo "<br>";
+echo $number ** $var;
+echo "<hr>";
+echo 11 ** 0.5;
+echo "<br>";
+echo $var . "<br>";
+echo $var++ . "<br>";
+echo $var;
+echo "<hr>";
+
+echo "<hr>";
+$var1 = 1;
+$var2 = 11;
+echo ++$var1 . "<br>";
+
+echo "<hr>";
+echo "<hr>";
+echo "<hr>";
+echo "<hr>";
 ?>
